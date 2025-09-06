eef41d872c76abf7785e535660b97cae7b6bf169
Fredrik93 <fredrikullman93@gmail.com>
2025-09-06 18:21:37 +0200

    initial

---
+++ src/main/java/com/teily/backend/TeilyApplication.java
@@ -0,0 +1,13 @@
+package com.teily.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeilyApplication.class, args);
+	}
+
+}
