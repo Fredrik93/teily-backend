d82fb37c8ac3d158e1c164949dc844bcaea0ff4f
Fredrik93 <fredrikullman93@gmail.com>
2025-10-25 15:26:34 +0200

    add ping endpoint

--- src/main/java/com/teily/backend/configuration/SecurityConfig.java
+++ src/main/java/com/teily/backend/configuration/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/actuator/**"
+                                "/actuator/**",
+                                "/ping"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
